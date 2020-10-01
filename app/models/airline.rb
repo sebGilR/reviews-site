ba18fe5914df03e7a2f8f8b7311353f5659afa05
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-10-01 13:34:56 -0500

    Set up basic model methods and seeds

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,13 @@
 class Airline < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+
+  before_create :slugify
+
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def average_score
+    reviews.average(:score).round(2).to_f
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+airlines = Airline.create([
+  { 
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  }, 
+  { 
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+  },
+  { 
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+  }, 
+  { 
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+  }, 
+  { 
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png" 
+  }, 
+  { 
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png" 
+  }
+])
+
+Review.create([
+  {
+    title: 'Great thing here',
+    description: 'Airline description',
+    score: 5,
+    airline: airlines.first
+  },
+  {
+    title: 'GrBadat thing here',
+    description: 'Airline description rev',
+    score: 1,
+    airline: airlines.first
+  }
+])
